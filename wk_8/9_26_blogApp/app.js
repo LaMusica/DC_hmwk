7aa2cb65c6a239f01e77fb65ffb5dbee74f85f26
LaMusica <aoduwole7@gmail.com>
2020-11-09 20:16:26 -0500

    ui renedering corrections

--- wk_8/9_26_blogApp/app.js
+++ wk_8/9_26_blogApp/app.js
@@ -1,9 +1,9 @@
-const express = resquire('express');
+const express = require('express');
 const app = express();
 const pgp = require('pg-promise')()
 const mustacheExpress = require('mustache-express');
 
-const connectionString = "postgres://localhost:5432/blogDB"
+const connectionString = "postgres://localhost:5432/blogdb"
 
 const db = pgp(connectionString);
 
@@ -14,18 +14,20 @@ app.set('views','./views');
 
 app.set('view engine', "mustache");
 
+
+/*
 let blogEntry = [{
-        "postID":postID,
+        //"postID":postID,
         "title":title,
         "body":body,
         "userName":userName
 }]
 
-
+*/
 
 app.get('/' , (req,res) =>{
   //  db.any('SELECT * FROM blog_posts;')
-      db.any('SELECT post_id, user_name, title, body FROM blog_posts;')
+      db.any('SELECT post_id,title, body FROM blog_post;')
 .then(blogs => {
 
         res.render("index", {blogs:blogs})
@@ -37,7 +39,7 @@ app.post('/delete-post', (req,res)=> {
 
         const postID = req.body.postID
         console.log(postID);
-        db.none("DELETE FROM blog_posts WHERE post_id = $1;", [postID])
+        db.none("DELETE FROM blog_post WHERE post_id = $1;", [postID])
         .then(() => {
                 res.redirect('/');
         })
@@ -48,7 +50,8 @@ app.post('/create-post', (req,res) => {
 
         const title = req.body.title;
         const body = req.body.body;
-        db.none('INSERT INTO blog_posts(title,body) VALUES($1,$2)', [title,body]) 
+        db.none('INSERT INTO blog_post(title,body) VALUES($1,$2)', [title,body]) 
+      //db.none('INSERT INTO blog_post(title,body) VALUES("Overcoming Adversity", "100 + Characters")  
         .then(() => {
             res.redirect("/")
         })
