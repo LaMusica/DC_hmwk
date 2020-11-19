c24dcb714e6209198145faff6f8a543aec1989f9
LaMusica <aoduwole7@gmail.com>
2020-11-19 12:48:41 -0500

    "app setup"

---
+++ wk_7/10_20TripsApp/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mustacheExpress= require('mustache-express');
+// require('process');
+const tripsRouter = require("./routes/trips"); 
+
+global.trips = [];
+
+app.use('/static', express.static('css'));
+
+app.use(express.static('images'));
+
+// app.use(express.urlencoded())
+
+app.use('/trips', tripsRouter)
+
+app.engine('mustache', mustacheExpress());
+
+app.set('views', "./views");
+
+
+
+
+
+
